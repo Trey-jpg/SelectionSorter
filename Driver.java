0207914c9ecc9f66a519fc21c4521ed8763b2d14
Trey-jpg <56291093+Trey-jpg@users.noreply.github.com>
2020-10-06 12:33:27 -0500

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,13 @@
+public class Driver {
+  
+    //entire Driver to hold in the array values and printArray  
+    public static void main(String[] args) 
+    { 
+        Sorter ob = new Sorter(); 
+        int[] arr = {10,11,6,43,2,78}; 
+        ob.sort(arr); 
+        ob.printArray(arr); 
+    } 
+} 
+    
+
